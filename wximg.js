8bd53ca71fe0cea1679e51544324b3032bb3d323
stone20 <32875841+stone20@users.noreply.github.com>
2017-10-22 23:49:08 -0500

    Add files via upload

---
+++ wximg.js
@@ -0,0 +1,69 @@
+tips = new Array(31);
+
+tips[0] = '<span class="wx">zyls1317</span>';
+tips[1] = '<span class="wx">zyls1407</span>';
+tips[2] = '<span class="wx">zyls1103</span>';
+tips[3] = '<span class="wx">zyls1212</span>';
+tips[4] = '<span class="wx">zyls1318</span>';
+tips[5] = '<span class="wx">zyls1215</span>';
+tips[6] = '<span class="wx">zyls1503</span>';
+tips[7] = '<span class="wx">zyls1504</span>';
+tips[8] = '<span class="wx">zyls1506</span>';
+tips[9] = '<span class="wx">zyls1507</span>';
+tips[10] = '<span class="wx">fanfan19910718</span>';
+tips[11] = '<span class="wx">zyls1319</span>'
+tips[12] = '<span class="wx">zyls1206</span>'
+tips[13] = '<span class="wx">zyls1207</span>'
+tips[14] = '<span class="wx">dysls008</span>';
+tips[15] = '<span class="wx">dysls004</span>';
+tips[16] = '<span class="wx">dysls005</span>';
+tips[17] = '<span class="wx">dysls003</span>';
+tips[18] = '<span class="wx">dysls010</span>';
+tips[19] = '<span class="wx">ertong0106</span>';
+tips[20] = '<span class="wx">dysls013</span>';
+tips[21] = '<span class="wx">dysls009</span>';
+tips[22] = '<span class="wx">tjbs101</span>';
+tips[23] = '<span class="wx">tjbs102</span>';
+tips[24] = '<span class="wx">tjbs103</span>';
+tips[25] = '<span class="wx">tjbs105</span>';
+tips[26] = '<span class="wx">tjbs110</span>';
+tips[27] = '<span class="wx">tjbs111</span>';
+tips[28] = '<span class="wx">tjbs112</span>';
+tips[29] = '<span class="wx">tjbs113</span>';
+tips[30] = '<span class="wx">tjbs114</span>';
+
+
+tipsss = new Array(31);
+tipsss[0] = '<img class="tu" src="zyls1317.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/zyls1317.jpg*/ border="0"  />';
+tipsss[1] = '<img class="tu" src="zyls1407.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/zyls1407.jpg*/ border="0"  />';
+tipsss[2] = '<img class="tu" src="zyls1103.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/zyls1103.jpg*/ border="0"  />';
+tipsss[3] = '<img class="tu" src="zyls1212.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/zyls1212.jpg*/ border="0"  />';
+tipsss[4] = '<img class="tu" src="zyls1318.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/zyls1318.jpg*/ border="0"  />';
+tipsss[5] = '<img class="tu" src="zyls1215.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/zyls1215.jpg*/ border="0"  />';
+tipsss[6] = '<img class="tu" src="zyls1503.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/zyls1503.jpg*/ border="0"  />';
+tipsss[7] = '<img class="tu" src="zyls1504.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/zyls1504.jpg*/ border="0"  />';
+tipsss[8] = '<img class="tu" src="zyls1506.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/zyls1506.jpg*/ border="0"  />';
+tipsss[9] = '<img class="tu" src="zyls1507.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/zyls1507.jpg*/ border="0"  />';
+tipsss[10] = '<img class="tu" src="fanfan19910718.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/fanfan19910718.jpg*/ border="0"  />';
+tipsss[11] = '<img class="tu" src="zyls1319.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/zyls1319.jpg*/ border="0"  />';
+tipsss[12] = '<img class="tu" src="zyls1206.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/zyls1206.jpg*/ border="0"  />';
+tipsss[13] = '<img class="tu" src="zyls1207.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/zyls1207.jpg*/ border="0"  />';
+tipsss[14] = '<img class="tu" src="dysls008.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/dysls008.jpg*/ border="0"  />';
+tipsss[15] = '<img class="tu" src="dysls004.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/dysls004.jpg*/ border="0"  />';
+tipsss[16] = '<img class="tu" src="dysls005.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/dysls005.jpg*/ border="0"  />';
+tipsss[17] = '<img class="tu" src="dysls003.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/dysls003.jpg*/ border="0"  />';
+tipsss[18] = '<img class="tu" src="dysls010.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/dysls010.jpg*/ border="0"  />';
+tipsss[19] = '<img class="tu" src="dysls001.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/dysls001.jpg*/ border="0"  />';
+tipsss[20] = '<img class="tu" src="dysls013.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/dysls013.jpg*/ border="0"  />';
+tipsss[21] = '<img class="tu" src="dysls009.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/dysls009.jpg*/ border="0"  />';
+tipsss[22] = '<img class="tu" src="tjbs101.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/tjbs101.jpg*/ border="0"  />';
+tipsss[23] = '<img class="tu" src="tjbs102.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/tjbs102.jpg*/ border="0"  />';
+tipsss[24] = '<img class="tu" src="tjbs103.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/tjbs103.jpg*/ border="0"  />';
+tipsss[25] = '<img class="tu" src="tjbs105.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/tjbs105.jpg*/ border="0"  />';
+tipsss[26] = '<img class="tu" src="tjbs110.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/tjbs110.jpg*/ border="0"  />';
+tipsss[27] = '<img class="tu" src="tjbs111.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/tjbs111.jpg*/ border="0"  />';
+tipsss[28] = '<img class="tu" src="tjbs112.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/tjbs112.jpg*/ border="0"  />';
+tipsss[29] = '<img class="tu" src="tjbs113.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/tjbs113.jpg*/ border="0"  />';
+tipsss[30] = '<img class="tu" src="tjbs114.jpg"/*tpa=http://yunfu.hzydwlkj.com/ang/tjbs114.jpg*/ border="0"  />';
+
+index = Math.floor(Math.random() * tips.length);
